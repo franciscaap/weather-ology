986703d3872de5158298bf23f6c17af22ab4b6a5
franciscaap <155903837+franciscaap@users.noreply.github.com>
2024-01-21 11:57:16 +0000

    Updated date/time and added search engine

---
+++ src/index.js
@@ -0,0 +1,32 @@
+function displayDate() {
+  let currentDate = document.querySelector("#current-time");
+  console.log(currentDate.innerHTML);
+  let now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes().toString().padStart(2, "0");
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  let day = days[now.getDay()];
+
+  currentDate.innerHTML = `${day} ${hours}:${minutes}`;
+}
+
+displayDate();
+
+function searchWeather(event) {
+  event.preventDefault();
+  let searchCity = document.querySelector(".form-search-input");
+  let city = document.querySelector("h1");
+  city.innerHTML = searchCity.value;
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", searchWeather);
